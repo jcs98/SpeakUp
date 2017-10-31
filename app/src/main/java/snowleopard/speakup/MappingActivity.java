6b2738e9fd3e87381a52eca3c18aa01ad3a4d1b6
abcf98 <amanframewala@gmail.com>
2017-11-01 00:02:33 +0530

    Profile Page With Stories

--- app/src/main/java/snowleopard/speakup/MappingActivity.java
+++ app/src/main/java/snowleopard/speakup/MappingActivity.java
@@ -52,7 +52,7 @@ import java.util.List;
  * Created by jcsha on 28-10-2017.
  */
 
-public class MappingActivity extends AppCompatActivity implements OnMapReadyCallback,
+public class MappingActivity extends FragmentActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
         LocationListener, GoogleMap.OnInfoWindowClickListener{
 
@@ -68,9 +68,6 @@ public class MappingActivity extends AppCompatActivity implements OnMapReadyCall
     private DatabaseReference mTitle;
     private String show_title;
     private FirebaseAuth mAuth;
-    private Double passedLat;
-    private Double passedLng;
-
 
 
     static private String Id;
@@ -91,9 +88,6 @@ public class MappingActivity extends AppCompatActivity implements OnMapReadyCall
         mLoc = (DatabaseReference) FirebaseDatabase.getInstance().getReference().child("Story");
         mAuth = FirebaseAuth.getInstance();
         mTitle = (DatabaseReference) FirebaseDatabase.getInstance().getReference().child("Story");
-
-        passedLat = Double.parseDouble(getIntent().getStringExtra("lat"));
-        passedLng = Double.parseDouble(getIntent().getStringExtra("long"));
 
     }
 
@@ -204,29 +198,9 @@ public class MappingActivity extends AppCompatActivity implements OnMapReadyCall
                         markerOptions.position(latLng);
                         markerOptions.title(Title);
                         markerOptions.snippet(Id);
-
-                        if(passedLat == Lat && passedLng == Long) {
-                            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
-                        }
-
-                        else {
-                            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET));
-                        }
+                        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET));
 
                         mMap.addMarker(markerOptions);
-
-                        if(passedLat == Lat && passedLng == Long) {
-//                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 21));
-//                            mMap.animateCamera(CameraUpdateFactory.zoomIn());
-//                            mMap.animateCamera(CameraUpdateFactory.zoomTo(21), 2000, null);
-
-                            //Build camera position
-                            CameraPosition cameraPosition = new CameraPosition.Builder()
-                                    .target(latLng)
-                                    .zoom(21).build();
-                            //Zoom in and animate the camera.
-                            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-                        }
 
 
                     }
@@ -239,13 +213,6 @@ public class MappingActivity extends AppCompatActivity implements OnMapReadyCall
             });
 
 
-//            if(passedLat != null) {
-//                LatLng passedlatLng = new LatLng(passedLat, passedLng);
-//
-//                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(passedlatLng, 20));
-//            }
-
-//            Toast.makeText(getApplicationContext(),passedLat.toString(), Toast.LENGTH_SHORT).show();
         }
 
     }
@@ -257,7 +224,6 @@ public class MappingActivity extends AppCompatActivity implements OnMapReadyCall
         Intent viewActivity = new Intent(MappingActivity.this,ViewStoryActivity.class);
         viewActivity.putExtra("Key",marker.getSnippet());
         startActivity(viewActivity);
-        finish();
 
     }
 
@@ -285,10 +251,10 @@ public class MappingActivity extends AppCompatActivity implements OnMapReadyCall
         markerOptions.title("Current Location");
         markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET));
 
-//        currentLocationMarker = mMap.addMarker(markerOptions);
+        currentLocationMarker = mMap.addMarker(markerOptions);
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12));
-//        mMap.animateCamera(CameraUpdateFactory.zoomBy(10));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomBy(10));
 
         if(client != null){
             LocationServices.FusedLocationApi.removeLocationUpdates(client, this);
--- app/src/main/java/snowleopard/speakup/ProfileActivity.java
+++ app/src/main/java/snowleopard/speakup/ProfileActivity.java
@@ -1,24 +1,43 @@
 package snowleopard.speakup;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class ProfileActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private DatabaseReference mDatabase;
+    private DatabaseReference mDatabase1;
+    private Query mQuery;
+
     private FirebaseUser mUser;
+    private RecyclerView mList;
+    private DatabaseReference mDatabaseUsers;
+
+
+
 
     private TextView mWelcome;
 
@@ -27,12 +46,19 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         mAuth = FirebaseAuth.getInstance();
+        mList = (RecyclerView) findViewById(R.id.list);
+        //mList.hasFixedSize(true);
+        mList.setLayoutManager(new LinearLayoutManager(this));
         mUser = mAuth.getCurrentUser();
-        String key = getIntent().getStringExtra("Key");
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(mAuth.getCurrentUser().getUid());
+        mDatabase1 = FirebaseDatabase.getInstance().getReference().child("Users").child(mAuth.getCurrentUser().getUid());
 
         mWelcome=   (TextView) findViewById(R.id.tvWelcome);
-        mDatabase.addValueEventListener(new ValueEventListener() {
+        mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users");
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Story");
+        mDatabase.keepSynced(true);
+        mDatabaseUsers.keepSynced(true);
+        mQuery = mDatabase.orderByChild("Owner").equalTo(mAuth.getCurrentUser().getUid());
+        mDatabase1.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
@@ -62,4 +88,158 @@ public class ProfileActivity extends AppCompatActivity {
         toast.show();
 
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseRecyclerAdapter<Cards_ListViewActivity , ListViewActivity.cardViewHolder> firebaserecycleradapter = new FirebaseRecyclerAdapter<Cards_ListViewActivity, ListViewActivity.cardViewHolder>(
+                Cards_ListViewActivity.class,
+                R.layout.list_row,
+                ListViewActivity.cardViewHolder.class,
+                mQuery
+                ) {
+
+
+            @Override
+            protected void populateViewHolder(final ListViewActivity.cardViewHolder viewHolder, final Cards_ListViewActivity model, int position) {
+
+                final String post_key = getRef(position).getKey();
+
+                viewHolder.setDescription(model.getDescription());
+
+                // mDatabaseUser = FirebaseDatabase.getInstance().getReference().child("Users").child(model.getOwner());
+
+                mDatabaseUsers.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        DataSnapshot owners = (dataSnapshot.child(model.getOwner()).child("name"));
+                        viewHolder.setOwner(owners.getValue().toString());
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+                viewHolder.setTitle(model.getTitle());
+                viewHolder.setImageUrl(getApplicationContext(), model.getImageURL());
+                //viewHolder.setLikeBtn(post_key);
+                viewHolder.mViewStory.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent viewActivity = new Intent(ProfileActivity.this, ViewStoryActivity.class);
+                        viewActivity.putExtra("Key", post_key);
+                        startActivity(viewActivity);
+                    }
+                });
+
+
+            }
+
+        };
+        mList.setAdapter(firebaserecycleradapter);
+
+    }
+    public static class cardViewHolder extends RecyclerView.ViewHolder {
+        View mView;
+        ImageButton mLikebtn;
+        TextView mNumLikes;
+        ImageButton  mViewStory;
+        DatabaseReference mDatabaseLike;
+        FirebaseAuth mAuth;
+
+
+        public cardViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+            mNumLikes = (TextView) mView.findViewById(R.id.mNLike);
+            mLikebtn = (ImageButton) mView.findViewById(R.id.like_btn);
+            mDatabaseLike=FirebaseDatabase.getInstance().getReference().child("Likes");
+            mViewStory = (ImageButton) mView.findViewById(R.id.imgCard);
+            mAuth=FirebaseAuth.getInstance();
+            mDatabaseLike.keepSynced(true);
+        }
+
+        public void setTitle(String title) {
+            TextView post_title = (TextView) mView.findViewById(R.id.tvTitle);
+            post_title.setText(title);
+
+        }
+        public void setLikeBtn(final String post_key){
+            mDatabaseLike.addValueEventListener(new ValueEventListener() {
+                @Override
+
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if(dataSnapshot.child(post_key).hasChild("NumberOfLikes")){
+                        mNumLikes.setText(dataSnapshot.child(post_key).child("NumberOfLikes").getValue().toString());}
+
+                    if(dataSnapshot.child(post_key).hasChild(mAuth.getCurrentUser().getUid())){
+                        mLikebtn.setImageResource(R.mipmap.likegray);
+
+                    }
+                    else{
+                        mLikebtn.setImageResource(R.mipmap.likeblack);
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+
+        public void setDescription(String description) {
+            TextView post_desc = (TextView) mView.findViewById(R.id.tvDesc);
+            post_desc.setText(description);
+        }
+        public void setOwner(String owner){
+
+            Button mOwnerbtn = (Button) mView.findViewById(R.id.owner_button);
+            mOwnerbtn.setText(owner);}
+
+        public void setImageUrl(Context ctx, String image){
+            ImageButton post_image = (ImageButton) mView.findViewById(R.id.imgCard);
+            Picasso.with(ctx).load(image).into(post_image);
+
+        }
+    }
+
+
+
+    @Override
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case  R.id.action_add:
+                Intent mainIntent = new Intent(ProfileActivity.this, AddStoryActivity.class);
+                startActivity(mainIntent);
+                return true;
+            case R.id.action_settings:
+                Intent intent = new Intent(ProfileActivity.this, ProfileActivity.class);
+                startActivity(intent);
+                return true;
+
+
+            case R.id.action_logout:
+
+//                Intent intent = new Intent(ListViewActivity.this, MainActivity.class);
+                mAuth.signOut();
+//                startActivity(intent);
+//                finish();
+                return true;
+
+
+            default:
+
+                return super.onOptionsItemSelected(item);
+
+        }
+
+    }
+
+
+
 }
